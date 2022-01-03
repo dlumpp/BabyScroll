94974d32f07df98cbdaa013302a532113619c7d9
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2022-01-03 13:13:21 -0500

    cleanup samples and failed experiments

--- app.js
+++ app.js
@@ -11,52 +11,24 @@ const App = {
             }
         ));
 
-
         return {
-            message: 'Hello Vue!!',
             items: rootItems
         }
     },
     methods: {
-        prepend() {
-            this.items.unshift(...Array.from({ length: 10 }).fill(String.fromCodePoint(0x1F34E)));
-        },
-        append() {
-            this.items.shift(...Array.from({ length: 10 }).fill(String.fromCodePoint(0x1F34E)));
-        },
         getScrollingElement() {
             return document.scrollingElement;
-        },
-        saveScrollPosition() {
-            console.log(this.$el.parentElement.scrollTop)
-            console.log(this.$el.parentElement.scrollHeight)
         },
         cloneItems() {
             this.items.unshift(...this.items);
         },
         handleScroll() {
-            /*
-            if almost out of track
-            lay track ahead or behind
-            */
             let rootEle = this.getScrollingElement();
             const shiftThreshhold = .8; // % of scroll height where we lay more track
             const numToShift = this.items.length / 2;
             if (rootEle.scrollTop > (rootEle.scrollHeight * shiftThreshhold)) {
                 this.scrollToStart();
-                //addBelow();
-                // const itemsToShift = this.items.slice(0, numToShift);
-                // // shift top half to bottom half
-                // this.items.push(...itemsToShift);
-                // this.$nextTick(function () {
-                //     this.items.splice(0, numToShift);
-                // });
             }
-
-            function addBelow() {
-                //rootEle.scrollTop = 3249;
-                this.scrollToStart();
-            };
         },
         scrollToStart() {
             // scroll to the 1st root item (middle item after cloning)
@@ -71,19 +43,6 @@ const App = {
         });
     },
     mounted() {
-        // this.observer = new IntersectionObserver(entries => {
-        //     entries.forEach(e => console.log('seent it'));
-        // });
-
-        // this.observer.observe(document.querySelector('.endMarker'));
-        // this.$nextTick(function () {
-        //     let rootEle = this.getScrollingElement();
-        //     const startingHeight = rootEle.scrollHeight;
-        //     this.cloneItems();
-        //     setTimeout(() => {
-        //         rootEle.scrollTop = rootEle.scrollHeight - startingHeight;
-        //     });
-        // });
 
         this.$nextTick(function () {
             this.cloneItems();
